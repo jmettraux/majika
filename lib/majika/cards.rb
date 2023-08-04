2b9290cb39e0e46cfe0a7158c4e125fd47b0c9dc
John Mettraux <jmettraux@gmail.com>
2023-08-04 20:26:18 +0900

    Integrate 3 images

--- lib/majika/cards.rb
+++ lib/majika/cards.rb
@@ -6,14 +6,31 @@ $: << 'lib'
 
 require 'majika/poems'
 
+def make_illustration(f)
+
+  svgs = Dir['web/images/choro/*.svg'].collect { |pa| pa.split('/', 2).last }
+
+  f.puts("<div class='illustration'>")
+  3.times do
+    f.puts("<div class='piece'>")
+    f.puts("<img src='#{svgs.sample}'>")
+    f.puts("</div>")
+  end
+  f.puts("</div>")
+end
+
 def make_card(f)
 
   s = make_poem(highlight_one_word: true)
 
   f.puts("<div class='card'>")
+
   f.puts("<div class='poem'>")
   f.puts(s.split("\n").join("<br/>"))
   f.puts("</div>")
+
+  make_illustration(f)
+
   f.puts("</div>")
 end
 
